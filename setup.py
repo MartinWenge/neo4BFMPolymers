223dd6bc6295e2843473ddc35f24886e108b2f4f
Martin Wenge <wengenmayr@ipfdd.de>
2020-11-02 11:35:22 +0100

    add some more details for package building

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE.txt') as f:
     license = f.read()
 
 setup(
-    name='neo4Polymer',
-    version='1.1',
+    name='neo4polymer',
+    version='1.1.0a1',
     description='Neo4j python-interface to manage computer simulation data, in particular for polymers',
     long_description=readme,
     long_description_content_type='text/markdown',
@@ -17,15 +17,26 @@ setup(
     author_email='martinwengenmayr@gmail.com',
     url='https://github.com/MartinWenge/neo4BFMPolymers',
     license=license,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Database',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research'
+    ]
     python_requires='>=3.0',
     install_requires=[
         'datetime',
         'os',
         'socket',
         're',
+        'abc',
+        'logging'
         'py2neo',
         'pandas',
         'numpy'
     ],
-    packages=find_packages(exclude=('tests', 'bfm_files', 'figures'))
+    packages=find_packages(where='neo4Polymer')
 )
